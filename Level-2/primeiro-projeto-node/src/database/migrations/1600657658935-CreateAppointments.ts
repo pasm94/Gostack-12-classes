a51049bc1c4819b6559a6cbf8ec2fd92ee2f8a1d
Paulo A. S. Müller <paulomuller94@gmail.com>
2020-09-30 22:37:10 -0300

    criado acesso ao database

--- Level-2/primeiro-projeto-node/src/database/migrations/1600657658935-CreateAppointments.ts
+++ Level-2/primeiro-projeto-node/src/database/migrations/1600657658935-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1600657658935
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
