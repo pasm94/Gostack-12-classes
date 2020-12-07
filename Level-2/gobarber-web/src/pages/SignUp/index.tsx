79c51b715f0d3804cd10c2bd25e306ab1230e361
Paulo A. S. Müller <paulomuller94@gmail.com>
2020-12-06 23:27:30 -0300

    finalizado exibicao de errors no input

--- Level-2/gobarber-web/src/pages/SignUp/index.tsx
+++ Level-2/gobarber-web/src/pages/SignUp/index.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-
+import React, { useCallback, useRef } from 'react'
 import { Container, Content, Background } from './styles'
+import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
-
+import * as Yup from 'yup'
 import logoImg from '../../assets/logo.svg'
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import Button from '../../components/button/index'
 import Input from '../../components/input/index'
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object) {
-    console.log(data);
+  const formRef = useRef<FormHandles>(null)
 
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({}) // precisa setar pq quando for sucesso ele n vai entrar no catch
+
+
+      const schema = Yup.object().shape({ // os dados que quero validar serao um objeto, e terao esse formato (shape)
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'), // nao precisa do required, pq se tem que ter 6 digitos
+        // no minimo, eh obvio que eh obrigatorio
+      })
+
+      await schema.validate(data, {
+        abortEarly: false, // retorna todos erros, inves de retornar apenas o primeiro
+      })
+
+    } catch (error) {
+      const errors = getValidationErrors(error)
+
+      formRef.current?.setErrors(errors)
+    }
+  }, [])
 
   return (
     <Container>
@@ -21,7 +42,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
