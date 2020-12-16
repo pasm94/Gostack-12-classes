320a2717e70450e78a8166b84acc079b35ef19b3
Paulo A. S. Müller <paulomuller94@gmail.com>
2020-12-15 23:24:47 -0300

    mantendo usuario no storage

--- Level-2/gobarber-web/src/App.tsx
+++ Level-2/gobarber-web/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import SignIn from './pages/SignIn/index'
-import SignUp from './pages/SignUp/index'
 import GlobalStyle from './styles/global'
-
 import { AuthProvider } from './context/AuthContext'
+
 
 const App: React.FC = () => (
   <>
     <AuthProvider> {/* tudo que estiver aqui dentro tera acesso a esse context */}
-      {/* <SignUp /> */}
       <SignIn />
     </AuthProvider>
     <GlobalStyle />
--- Level-2/gobarber-web/src/context/AuthContext.tsx
+++ Level-2/gobarber-web/src/context/AuthContext.tsx
@@ -1,8 +1,13 @@
-import React, { createContext, useCallback } from 'react'
+import React, { createContext, useCallback, useState } from 'react'
 
 import api from '../services/api'
 // criamos o contexto para pegar informações de outro lugar da aplicacao
 
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface SignInCredentials {
   email: string;
@@ -10,14 +15,26 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  name: string;
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
-const AuthProvider: React.FC = ({ children }) => {
+
+export const AuthProvider: React.FC = ({ children }) => {
   // tudo que esse componente receber como child, repassaremos em algum lugar aqui dentro
+
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token')
+    const user = localStorage.getItem('@GoBarber:user')
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) }
+    }
+
+    return {} as AuthState
+  })
 
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
@@ -25,15 +42,17 @@ const AuthProvider: React.FC = ({ children }) => {
       password,
     })
 
-    console.log(response.data);
-    
+    const { token, user } = response.data
+
+    localStorage.setItem('@GoBarber:token', token)
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user))
+
+    setData({ token, user })
   }, [])
 
   return (
-    <AuthContext.Provider value={{ name: 'Paulo', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   )
 }
-
-export { AuthContext, AuthProvider }
