3f19504e7b35bf2dafbcee99e26692d0eaa6869a
Paulo A. S. Müller <paulomuller94@gmail.com>
2020-12-18 19:51:29 -0300

    estrutura de toast feita

--- Level-2/gobarber-web/src/App.tsx
+++ Level-2/gobarber-web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import SignIn from './pages/SignIn/index'
 import GlobalStyle from './styles/global'
+
+import ToastContainer from './components/ToastContainer'
 import { AuthProvider } from './hooks/AuthContext'
 
 
@@ -9,6 +11,8 @@ const App: React.FC = () => (
     <AuthProvider> {/* tudo que estiver aqui dentro tera acesso a esse context */}
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer/>
     <GlobalStyle />
   </>
 )
--- Level-2/gobarber-web/src/pages/Signin/index.tsx
+++ Level-2/gobarber-web/src/pages/Signin/index.tsx
@@ -44,9 +44,13 @@ const SignIn: React.FC = () => {
         password: data.password,
       })
     } catch (error) {
-      const errors = getValidationErrors(error)
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error)
 
-      formRef.current?.setErrors(errors)
+        formRef.current?.setErrors(errors)
+      }
+
+      
     }
   }, [signIn]) // colocar as variaveis externas nas dependencias
 
