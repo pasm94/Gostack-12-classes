9eca00aa4868b2c69b9d36283b4cf07d1e9460e5
Paulo A. S. Müller <paulomuller94@gmail.com>
2020-12-26 01:54:44 -0300

    feita densidade de pixel e modulo de png

---
+++ Level-2/appgobarber/src/@types/index.d.ts
@@ -0,0 +1,2 @@
+declare module '*.png' 
+// assim arquivos png sao modulos p aplicacao, como arquivos ts, js... (Pra n dar erro ao importar esse tipo de arquivo)
--- Level-2/appgobarber/src/pages/SignIn/index.tsx
+++ Level-2/appgobarber/src/pages/SignIn/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Image } from 'react-native'
 
 import { Container } from './styles'
+import logoImg from '../../assets/logo.png' // @2x e @3x eh a questao de densidade de pixel (o dispositivo escolhe o certo)
 
 const SignIn: React.FC = () => {
   return (
-    <Container />
+    <Container>
+      <Image source={logoImg} />
+    </Container>
   )
 }
 
