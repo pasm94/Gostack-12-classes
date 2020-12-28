94d880c7ae6c2852e7aabfdeeb42391df697f033
Paulo A. S. Müller <paulomuller94@gmail.com>
2020-12-28 03:40:55 -0300

    feito foco e preenchimento

--- Level-2/appgobarber/src/components/Input/index.tsx
+++ Level-2/appgobarber/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react'
+import React, { useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback } from 'react'
 // useImperativeHandle eh usado para passar info do component child para component parent
 import { TextInputProps } from 'react-native'
 import { useField } from '@unform/core'
@@ -27,6 +27,19 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
 
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
 
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false)
+
+    setIsFilled(!!inputValueRef.current.value)
+  }, [])
+
   useImperativeHandle(ref, () => ({
     focus() { inputElementRef.current.focus() }
   })) // eh acessado uma informacao de um elemento child, pelo elemento parent, atraves do ref
@@ -48,13 +61,15 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
   }, [fieldName, registerField])
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value
         }}
