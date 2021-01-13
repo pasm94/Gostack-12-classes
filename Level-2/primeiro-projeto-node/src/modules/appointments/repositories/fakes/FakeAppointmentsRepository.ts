4ecec4a8898412dd2f67f9078366397895039204
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-01-12 23:37:45 -0300

    feito testes de agendamento

--- Level-2/primeiro-projeto-node/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ Level-2/primeiro-projeto-node/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '../IAppointmentsRepository';
 import ICreateAppointmentDTO from '../../dtos/ICreateAppointmentDTO';
@@ -8,8 +9,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
