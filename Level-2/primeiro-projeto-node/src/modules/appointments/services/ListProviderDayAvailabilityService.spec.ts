afd4e4572d75238d6e4fe6b011aa966e7448b191
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-01-26 10:04:45 -0300

    feito excluindo horarios antigos

--- Level-2/primeiro-projeto-node/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ Level-2/primeiro-projeto-node/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -40,4 +40,39 @@ describe('ListProviderDayAvailability', () => {
       ]),
     );
   });
+
+  it('should be able to list the day availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 3, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 3, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 3, 20, 11).getTime();
+    });
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id: 'user',
+      year: 2021,
+      month: 4,
+      day: 20,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 13, available: true },
+      ]),
+    );
+  });
 });
