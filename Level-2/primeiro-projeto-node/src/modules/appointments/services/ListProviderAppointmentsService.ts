a0f18bd42c2acecaa7b6a4ee227098fc479b8c43
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-01-31 21:23:32 -0300

    feito cache de agendamentos

--- Level-2/primeiro-projeto-node/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Level-2/primeiro-projeto-node/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,20 +27,25 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('key-test');
+    const cacheKey = `provider:appointments:${provider_id}:${year}-${month}-${day}`;
 
-    console.log(cacheData);
-
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        year,
-        month,
-        day,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    // await this.cacheProvider.save('key-test', 'value-test');
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      );
+      console.log('buscou do banco');
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
