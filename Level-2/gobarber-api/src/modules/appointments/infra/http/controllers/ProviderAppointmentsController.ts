5b4767af7c969788644c5860c64da7075b57b3ed
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-02-03 22:56:59 -0300

    feito serializacao no cache

--- Level-2/gobarber-api/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ Level-2/gobarber-api/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
@@ -19,6 +20,6 @@ export default class ProviderAppointmentsController {
       year: Number(year),
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
