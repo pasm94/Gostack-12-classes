29fc5b245a233775794590b1aa0fc1092002364c
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-02-04 02:47:56 -0300

    feito implementando redefinicao

--- Level-2/gobarber-web/src/pages/ResetPassword/index.tsx
+++ Level-2/gobarber-web/src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import { FiLock } from 'react-icons/fi'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import { Container, Content, Background, AnimationContainer } from './styles'
 import logoImg from '../../assets/logo.svg'
@@ -12,6 +12,7 @@ import Input from '../../components/Input/index'
 import getValidationErrors from '../../utils/getValidationErrors'
 import { useAuth } from '../../hooks/auth'
 import { useToast } from '../../hooks/toast'
+import api from '../../services/api'
 
 
 interface ResetPasswordFormData {
@@ -22,9 +23,10 @@ interface ResetPasswordFormData {
 const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { signIn } = useAuth()
   const { addToast } = useToast()
   const history = useHistory()
+
+  const location = useLocation()
 
   const handleSubmit = useCallback(async (data: ResetPasswordFormData) => {
     try {
@@ -42,7 +44,20 @@ const ResetPassword: React.FC = () => {
         abortEarly: false,
       })
 
-      history.push('/signin')
+      const { password, password_confirmation } = data
+      const token = location.search.replace('?token=', '')
+
+      if (!token) {
+        throw new Error()
+      }
+
+      await api.post('/password/reset', {
+        password,
+        password_confirmation,
+        token
+      })
+
+      history.push('/')
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         const errors = getValidationErrors(error)
@@ -58,7 +73,7 @@ const ResetPassword: React.FC = () => {
         description: 'Ocorreu um erro ao resetar sua senha, tente novamente.',
       })
     }
-  }, [addToast, history])
+  }, [addToast, history, location.search])
 
   return (
     <Container>
