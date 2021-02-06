40cb8c1c6a892d4a3feb329a9857ffd7212caa18
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-02-05 22:40:59 -0300

    feito proximo agendamento

--- Level-2/gobarber-web/src/pages/Dashboard/index.tsx
+++ Level-2/gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment
+} from './styles'
 import logoImg from '../../assets/logo.svg'
-import { FiPower } from 'react-icons/fi'
+import { FiClock, FiPower } from 'react-icons/fi'
 import { useAuth } from '../../hooks/auth'
 
 const Dashboard: React.FC = () => {
@@ -30,6 +39,35 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src={user.avatar_url}
+                alt=""
+              />
+              <strong>Paulo Alberto</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   )
 }
